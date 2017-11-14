b4ffebabdae28b68bcc19b2f0e3a783f728f4311
kepler.le <kepler.le@free.fr>
2017-11-14 21:21:05 +0100

    Resultat du Skype avec Djory
    Ca Maaaaarche !!!!! :D

--- src/main/java/com/kepler/controller/EleveController.java
+++ src/main/java/com/kepler/controller/EleveController.java
@@ -2,8 +2,10 @@ package com.kepler.controller;
 
 import com.kepler.model.Eleve;
 import com.kepler.service.EleveService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,60 +13,66 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.print.attribute.standard.Media;
 import java.util.List;
 
 
 @Controller("eleveController")
 @RequestMapping("/eleve")
 public class EleveController {
+    private static final Logger LOGGER = Logger.getLogger(EleveController.class);
     @Autowired
-    EleveService eleService;
+    EleveService eleveService;
 
-// GET qui permet de récupérer des informations identifié par l'URI de la demande
-@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-public ResponseEntity<Eleve> getEleve(@PathVariable("id") Long id) {
-   Eleve eleve = eleService.getById(id);
-    if (eleve == null) {
-        return new ResponseEntity<Eleve>(HttpStatus.NOT_FOUND);
+    // GET qui permet de récupérer des informations identifié par l'URI de la demande
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Eleve> getEleve(@PathVariable("id") Long id) {
+        Eleve eleve = eleveService.getById(id);
+        if (eleve == null) {
+            return new ResponseEntity<Eleve>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Eleve>(eleve, HttpStatus.OK);
+
     }
-    return new ResponseEntity<Eleve>(eleve, HttpStatus.OK);
 
-}
-
-    @RequestMapping(value="", method=RequestMethod.GET)
-    public ResponseEntity<List<Eleve>> getAllEleves(){
-        List<Eleve> eleves=eleService.getAll();
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity<List<Eleve>> getAllEleves() {
+        List<Eleve> eleves = eleveService.getAll();
         if (eleves.isEmpty()) {
             return new ResponseEntity<List<Eleve>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Eleve>>(eleves, HttpStatus.OK);
     }
 
-   // POST demande au server d'origine d'accepter l'entité incluse dans la requete ent tant que nouveau subordonné de la ressource identifié par l'URI
-    @RequestMapping(value="", method = RequestMethod.POST)
-    public ResponseEntity<Eleve> addEleve(@RequestBody Eleve eleve){
-        eleService.save(eleve);
-        ResponseEntity<Eleve> responseEntity = new ResponseEntity<Eleve>(eleve,HttpStatus.CREATED);
+    // POST demande au server d'origine d'accepter l'entité incluse dans la requete ent tant que nouveau subordonné de la ressource identifié par l'URI
+    @RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Eleve> addEleve(@RequestBody Eleve eleve) {
+        //LOGGER.info("POST effectué");
+        System.out.println("POST effectué");
+        eleveService.save(eleve);
+        ResponseEntity<Eleve> responseEntity = new ResponseEntity<Eleve>(eleve, HttpStatus.CREATED);
         return responseEntity;
+
     }
-    @RequestMapping(value="", method=RequestMethod.PUT)
-    public ResponseEntity<Eleve> updateEleve(@RequestBody Eleve eleve){
-        Eleve existingEle = eleService.getById(eleve.getId());
+
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public ResponseEntity<Eleve> updateEleve(@RequestBody Eleve eleve) {
+        Eleve existingEle = eleveService.getById(eleve.getId());
         if (existingEle == null) {
             return new ResponseEntity<Eleve>(HttpStatus.NOT_FOUND);
         } else {
-            eleService.save(eleve);
+            eleveService.save(eleve);
             return new ResponseEntity<Eleve>(HttpStatus.OK);
         }
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Void> deleteEleve(@PathVariable("id") Long id) {
-       Eleve eleve = eleService.getById(id);
+        Eleve eleve = eleveService.getById(id);
         if (eleve == null) {
             return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
         } else {
-            eleService.delete(id);
+            eleveService.delete(id);
             return new ResponseEntity<Void>(HttpStatus.GONE);
 
         }
